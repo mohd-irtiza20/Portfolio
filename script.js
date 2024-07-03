a9ab7d87f0418f3210a7294645ffae5007a2f696
mohd-irtiza20 <mohdirtiza20@gmail.com>
2024-07-03 13:17:13 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+  var menuToggle = document.querySelector(".toggle");
+  menuToggle.classList.toggle("active");
+
+  var menu = document.querySelector(".head-menu");
+  menu.classList.toggle("active");
+}
+
